4ddc84f35717de92076f3910e7a5a6e153dc1fc1
NK-108 <nkmail1221@gmail.com>
2025-11-16 20:06:07 -0500

    more aliases; qutebrowser editor config

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -2,6 +2,8 @@ import pywalQute.draw
 
 config.load_autoconfig()
 
+c.editor.command = ["foot", "nvim","-f", "{file}", "-c", "normal {line}G{column0}1"]
+
 pywalQute.draw.color(c, {
     'spacing': {
         'vertical': 6,
